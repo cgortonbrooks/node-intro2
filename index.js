bcf000d5f43e5e44102ab9865d91be8ebfd58a2c
Craig Gorton <cgorton@brooksschool.org>
2026-04-23 15:08:41 -0400

    added database

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mysql = require('mysql2')
 const app = express()
 const PORT = 3000
+
+const db = mysql.createConnection({
+    host: '66.198.240.46',
+    user: 'bfzhiwes_node-intro-user',
+    password: 'node-intro-user-password',
+    database: 'bfzhiwes_node-intro'
+})
+
+app.get('/students', (req, res) => {
+    let sql = 'select * from students'
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).send(err)
+        res.json(results)
+    })
+})
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello Mr Gorton</h1>')
